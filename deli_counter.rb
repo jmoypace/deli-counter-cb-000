a69b877d0bfb3cc5ce92f4ee20604e9aa2d9ab02
jmoypace <jmoypace@gmail.com>
2018-06-26 01:00:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,9 @@ katz_deli = []
 def line(katz_deli)
   newarr=[]
   counter=1
-if (katz_deli.length>0)
+if (katz_deli.length==0)
+  return "The line is currently empty"
+else
   katz_deli.each do|person|
     newarr.push(" #{counter}. #{person}")
     counter+=1
