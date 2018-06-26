cb725971781b7d17a1b1154717bafdca3aba4704
jmoypace <jmoypace@gmail.com>
2018-06-26 01:14:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,8 @@ end
 
 def take_a_number(katz_deli, name)
 katz_deli.push(name)
-
+#"Welcome, Grace. You are number 4 in line."
+puts "Welcome, #{katz_deli.last}. You are number #{(katz_deli.length)+1} in line."
 
 end
 
